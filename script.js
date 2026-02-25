4ad999af50cccc910c50fbae6b70d8e4ca7e3f51
ThTeras <142935713+ThTeras@users.noreply.github.com>
2026-02-24 23:20:49 -0300

    agora sim tá calculando direito
    Bagulho ficou foi doido

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-let valorT = document.getElementById("valorT"),
-quantidade = document.getElementById("quantidade"),
-descontoT = document.getElementById("descontoT"),
-resultado = document.getElementsByClassName("resultado");
-function calcular() {
-    let valorU = Number(valorT.value) / Number(quantidade.value);
-    let descontoU = Number(descontoT.value) / Number(quantidade.value);
-    let valorC = valorU + descontoU;
+//altera o estilo
 
-    if ((valorC * quantidade) - valorT != 0) {
-        valorC += ((valorC * quantidade) - valorT) / quantidade
+function inverter(){
+    let estiloAtual = document.getElementsByTagName("link")[2];
+    console.log(estiloAtual);
+    if(estiloAtual.getAttribute("href") == "style/light.css"){
+        estiloAtual.setAttribute("href","style/dark.css");
+    }else if(estiloAtual.getAttribute("href") == "style/dark.css"){
+        estiloAtual.setAttribute("href","style/light.css");
+    }
+}
+
+//calcular e mostrar valor
+
+const real = new Intl.NumberFormat("pt-BR",{
+    style:"currency",
+    currency:"BRL",
+})
+
+function calcular() {
+    let valorT = document.getElementById("valorT").value,
+    quantidade = document.getElementById("quantidade").value,
+    descontoT = document.getElementById("descontoT").value,
+    resultado = document.getElementsByClassName("resultado");
+
+    let valorU = valorT / quantidade;
+    let descontoU = descontoT / quantidade;
+    let valorC = valorU + descontoU;
+    let valorTC = valorC * quantidade;
+
+    if ((valorTC - descontoT) - valorT != 0) {
+        valorC += (valorT - valorTC) / quantidade;
+        valorTC = valorC * quantidade;
     }
 
-    resultado[0].innerHTML = "falta";
-    resultado[1].innerHTML = "falta";
-    resultado[2].innerHTML = "falta";
+    resultado[0].innerHTML = real.format(valorC);
+    resultado[1].innerHTML = real.format(valorTC);
+    resultado[2].innerHTML = real.format(valorTC - descontoT);
 }
